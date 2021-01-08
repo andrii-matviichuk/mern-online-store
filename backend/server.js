9c11839c15f824dd7f8aed487511ce4a5e78bf14
Andrii Matviichuk <matviichuk.andr@gmail.com>
2021-01-08 14:44:55 +0100

    Backend initial setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((prod) => prod._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, () => console.log('App is running on port 5000'));
